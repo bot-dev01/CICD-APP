13e2363ad14a67336332e1639693dd81084d62ae
bot-dev01 <ozydevkapil10@gmail.com>
2026-04-26 12:34:53 +0530

    Updated Final Text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🔥 CI/CD is LIVE bro!");
+  res.send("CI/CD is LIVE! and it took a hell lot of time to make it happen. well this txt is visible in the output no one cares about. But it really took a lot of time to make it happen.");
 });
 
 const PORT = process.env.PORT || 3000;
